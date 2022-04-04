476c64dacb1478c44016bde1aff3ae02c0a68357
BungaEdu <eduardoflores.9595@gmail.com>
2022-04-05 00:01:56 +0200

    casa lunes 04.04

--- ej1Matrices.java
+++ ej1Matrices.java
@@ -1,22 +1,32 @@
+import java.util.Scanner;
+
 public class ej1Matrices {
-    public static void main(String[] Arg) throws java.io.IOException {
-     /*   int [][] matrizResultado = {{1,2,3,4},{10,20,30,40}};
-        
-        for (int x=0; x < matrizResultado.length; x++) {
-              for (int y=0; y < matrizResultado[x].length; y++) {
-                System.out.println(matrizResultado[0]);
-                }
-        }
-        
-        */
-        int[][] matriz = {{2, 4, 4}, {6, 6, 9}, {8, 10, 12}};
+    public static void main(String[] Arg){
+        Scanner sc = new Scanner(System.in);
 
-        for (int x = 0; x < matriz.length; x++) {
-            System.out.println("\n");
-            for (int y = 0; y < matriz[x].length; y++) {
-                System.out.println(matriz[x][y]);
+        System.out.println("Monto inicial: ");
+        int montoInicial = sc.nextInt();
+        int TAM=0;
+        int [][] arrayIntereses = new int [TAM];
+
+        int intereses=0;
+        System.out.println("Intereses (pulsa 0 después del úlimo interés: ");
+        intereses = sc.nextInt();
+        while (intereses!=0) {
+            intereses = sc.nextInt();
+        };
+
+
+
+        /**************************** BUCLE PARA MOSTRAR POR PANTALLA
+         int[][] matriz = {{2, 4, 4}, {6, 6, 9}, {8, 10, 12}};
+         for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
+                System.out.print(matriz[i][j] + " ");	// Imprime elemento
             }
+            System.out.println();	// Imprime salto de línea
 
         }
+        */
     }
 }
